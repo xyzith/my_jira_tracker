8942a75dccb6ae81d7b752bffd785e2fafcdfeb4
Xyzith <taylor.t@tc-gaming.com>
2020-01-20 18:32:14 +0800

    add issue status

--- src/popup/config.mjs
+++ src/popup/config.mjs
@@ -1,5 +1,5 @@
 export const statusMap = new Map();
-statusMap.set(['Open'], { type: 'open' });
+statusMap.set(['Open', 'Action Needed'], { type: 'open' });
 statusMap.set(['To Do'], { type: 'todo' });
 statusMap.set(['In Progress'], { type: 'wip' });
 statusMap.set(['Resolved', 'Scheduled', 'In Review'], { type: 'resolved' });
